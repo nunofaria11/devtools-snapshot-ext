186730919d9f2a66b839f133bd6ec168b441cc87
nunofaria11 <2145649+nunofaria11@users.noreply.github.com>
2020-05-19 10:20:03 +0100

    WIP: capture all logging from page context

--- source/content.js
+++ source/content.js
@@ -1,7 +1,49 @@
+import Logger from './app/logger';
+import Constants from './app/constants';
+import Messaging from './app/messaging';
+
+Logger.enabled = false;
+
+let windowMessageDeferred = null;
+
+Messaging.registerMessageHandler(Constants.Messages.CONSOLE_ENTRIES, handleConsoleEntriesRequest);
+
+async function handleConsoleEntriesRequest() {
+
+    // Request the page context for logEntries
+    const logEntries = await requestPageLogEntries();
+
+    return Promise.resolve(logEntries);
+}
+
+async function requestPageLogEntries() {
+    return new Promise((resolve, reject) => {
+
+        const request = { name: 'req:devtools-snapshot_log-entries' };
+        window.postMessage(request, window.origin);
+
+        windowMessageDeferred = { resolve, reject };
+    });
+}
+
+window.addEventListener('message', (evt) => {
+    if (evt.source !== window || !evt.data) {
+        return;
+    }
+    const response = evt.data;
+    if (response.name !== 'res:devtools-snapshot_log-entries') {
+        return;
+    }
+    if (windowMessageDeferred) {
+        windowMessageDeferred.resolve(response.logEntries || []);
+        windowMessageDeferred = null;
+    }
+});
+
 /*
- * Adapted from:
- * https://stackoverflow.com/questions/9515704/insert-code-into-the-page-context-using-a-content-script
- */
+    * Adapted from:
+    * https://stackoverflow.com/questions/9515704/insert-code-into-the-page-context-using-a-content-script
+    */
 function scriptFromFile(file) {
     const script = document.createElement("script");
     script.src = browser.extension.getURL(file);
@@ -27,11 +69,41 @@ function inject(parentNode, scripts) {
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const documentHead = document.querySelector('head');
+function onDocumentHeadReady() {
     const scriptFiles = [
         'content_inject.js'
     ];
     const scripts = scriptFiles.map(scriptFromFile);
-    inject(documentHead, scripts);
-});
+    inject(document.head || document.documentElement, scripts);
+}
+
+// function waitForDocumentHead(readyCallback) {
+//     // Select the node that will be observed for mutations
+//     const targetNode = document.all[0];
+
+//     // Options for the observer (which mutations to observe)
+//     const config = { childList: true, subtree: false };
+
+//     // Callback function to execute when mutations are observed
+//     const mutationCallback = function (mutationsList, observer) {
+//         for (let mutation of mutationsList) {
+//             if (mutation.type === 'childList' && mutation.addedNodes) {
+//                 for (let newNode of mutation.addedNodes) {
+//                     if (newNode.nodeName.toLowerCase() === 'head') {
+//                         readyCallback();
+//                         observer.disconnect();
+//                     }
+//                 }
+//             }
+//         }
+//     };
+
+//     // Create an observer instance linked to the callback function
+//     const observer = new MutationObserver(mutationCallback);
+
+//     // Start observing the target node for configured mutations
+//     observer.observe(targetNode, config);
+// }
+
+// waitForDocumentHead(onDocumentHeadReady);
+onDocumentHeadReady();
--- source/content_inject.js
+++ source/content_inject.js
@@ -1,5 +1,4 @@
-import Constants from './app/constants';
-import Messaging from './app/messaging';
+
 import Logger from './app/logger';
 import consoleHook from 'console-hook';
 
@@ -8,15 +7,35 @@ Logger.enabled = false;
 const snapshotConsoleModel = {
 	logEntries: []
 };
-const attachedConsoleHook = consoleHook().attach((method, args) => {
-	const logEntry = {method, args, t: Date.now()};
+const attachedConsoleHook = consoleHook().attach((method, consoleArgs) => {
+	let args;
+	if (consoleArgs) {
+		args = [];
+		for (let a of consoleArgs) {
+			args.push(a);
+		}
+	}
+	const logEntry = { method, args, t: Date.now() };
 	snapshotConsoleModel.logEntries.push(logEntry);
 });
 
-async function handleConsoleEntriesRequest() {
-	return Promise.resolve(snapshotConsoleModel.logEntries);
-}
-Messaging.registerMessageHandler(Constants.Messages.CONSOLE_ENTRIES, handleConsoleEntriesRequest);
 
-window.addEventListener('beforeunload', () => attachedConsoleHook.detach());
+window.addEventListener('message', (evt) => {
+	if (evt.source !== window || !evt.data) {
+		return;
+	}
+	const request = evt.data;
+	if (request.name !== 'req:devtools-snapshot_log-entries') {
+		return;
+	}
 
+	const response = {
+		name: 'res:devtools-snapshot_log-entries',
+		logEntries: snapshotConsoleModel.logEntries
+	};
+	evt.source.postMessage(response);
+});
+
+window.addEventListener('beforeunload', () => {
+	attachedConsoleHook.detach()
+});
